8543311e0e87152c2dd74db1296c4f491b93e879
Marina8989 <marina.mill209@gmail.com>
2021-06-27 12:15:44 -0700

    react menu project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+
+function App() {
+    return (
+        <div>
+            <h2>Hello</h2>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import data from './data';
+
+const Menu = () => {
+    return (
+        <div>
+            <h3>Menu...</h3>
+        </div>
+    )
+}
+
+export default Menu
